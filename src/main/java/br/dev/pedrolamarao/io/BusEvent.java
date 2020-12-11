ac26010e4768f3ad880099f69bd0121c5afd7523
Pedro Lamarão <pedro.lamarao@prodist.com.br>
2020-12-11 16:47:37 -0300

    Define toString

--- src/main/java/br/dev/pedrolamarao/io/BusEvent.java
+++ src/main/java/br/dev/pedrolamarao/io/BusEvent.java
@@ -5,4 +5,9 @@ import jdk.incubator.foreign.MemoryAddress;
 @SuppressWarnings("preview")
 public final record BusEvent(long key, MemoryAddress operation, boolean status, int data) 
 {
+	@Override
+	public String toString ()
+	{
+		return String.format("{ handler = %d, operation = %d, status = %b, data = %d }", key, operation.toRawLongValue(), status, data);
+	}
 }
